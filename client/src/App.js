a85c230347caea6ba5103f611e1cdb34adaaa593
Brian Weitz <brianweitz@Brians-MacBook-Pro-2.local>
2019-08-20 14:38:55 -0400

    init

---
+++ client/src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h2>Macro Manager</h2>
+      <a href="https://macromanager.surge.sh" target="_blank">Link</a>
+      <h2>Events Express</h2>
+      <a href="https://events-express.surge.sh" target="_blank">Link</a>
+      <h2>Barrel-Aged</h2>
+      <a href="https://barrel-aged.surge.sh" target="_blank">Link</a>
+      <h2>Caracas</h2>
+      <a href="https://caracas-restaurant.surge.sh" target="_blank">Link</a>
+      <h2>Curtain Call</h2>
+      <a href="https://curtain-call.surge.sh" target="_blank">Link</a>
+    </div>
+  );
+}
+
+export default App;

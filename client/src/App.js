b27d24e9d41375e61f84aea00312bcdfb57f361d
Brian Weitz <brianweitz@Brians-MacBook-Pro-2.local>
2019-09-03 16:01:19 -0400

    full style first round

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import SkillPics from './components/SkillPics';
 import Portfolio from './components/Portfolio';
+import profile from './assets/profile.jpeg';
 
 function App() {
   return (
@@ -9,12 +10,15 @@ function App() {
       <div id="header">
         <h1>Brian Evan Weitz</h1>
         <h3>Software Engineer</h3>
-        <a href="#about">About</a>
-        <a href="#portfolio">Portfolio</a>
-        <a href="#contact">Contact</a>
+        <div id="nav">
+          <a href="#about">About</a>
+          <a href="#portfolio">Portfolio</a>
+          <a href="#contact">Contact</a>
+        </div>
       </div>
       <div id="about">
         <h3>About me</h3>
+        <img id="profile-pic" src={profile} />
         <p>Web developer with a background in theater and immersive storytelling. I create compelling, responsive, full stack web applications.</p>
         <SkillPics />
       </div>
@@ -23,6 +27,11 @@ function App() {
       </div>
       <div id="contact">
         <h3>Contact</h3>
+        <div id="contact-links">
+          <a href="mailto:brianevanweitz@gmail.com">Email</a>
+          <a href="https://www.linkedin.com/in/brianweitz1088/">LinkedIn</a>
+          <a href="https://github.com/brianevanweitz">GitHub</a>
+        </div>
       </div>
     </div>
   );
--- client/src/components/Portfolio.jsx
+++ client/src/components/Portfolio.jsx
@@ -6,15 +6,40 @@ import macro from '../assets/macro.png';
 
 export default function Portfolio() {
   return (
-    <div id="portfolio-section">
-      <h2>Macro Manager</h2>
-      <a href="https://macromanager.surge.sh" target="_blank">Link</a>
-      <h2>Events Express</h2>
-      <a href="https://events-express.surge.sh" target="_blank">Link</a>
-      <h2>Barrel-Aged</h2>
-      <a href="https://barrel-aged.surge.sh" target="_blank">Link</a>
-      <h2>Caracas</h2>
-      <a href="https://caracas-restaurant.surge.sh" target="_blank">Link</a>
-    </div>
+    <>
+      <h3>Projects</h3>
+      <div id="portfolio-section">
+        <div className="website web-macro">
+          <div className="hover-box">
+            <h2>Macro Manager</h2>
+            <a href="https://macromanager.surge.sh" target="_blank">Web Link</a>
+          </div>
+        </div>
+        <div className="website web-events">
+          <div className="hover-box">
+            <h2>Events Express</h2>
+            <a href="https://events-express.surge.sh" target="_blank">Web Link</a>
+          </div>
+        </div>
+        <div className="website web-barrel">
+          <div className="hover-box">
+            <h2>Barrel-Aged</h2>
+            <a href="https://barrel-aged.surge.sh" target="_blank">Web Link</a>
+          </div>
+        </div>
+        <div className="website web-caracas">
+          <div className="hover-box">
+            <h2>Caracas</h2>
+            <a href="https://caracas-restaurant.surge.sh" target="_blank">Web Link</a>
+          </div>
+        </div>
+        <div className="website web-curtain">
+          <div className="hover-box">
+            <h2>Curtain Call</h2>
+            <a href="https://curtain-call.surge.sh" target="_blank">Web Link</a>
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
